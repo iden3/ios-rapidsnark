7aed81660b4b6eb83b008c9c7124e92d19467c19
Yaroslav Moria <5eeman@users.noreply.github.com>
2024-09-06 16:09:58 +0200

    Updated rapidsnark to 0.0.4. Updated version name.

---
+++ Example/rapidsnark/witness_calc.h
@@ -0,0 +1,17 @@
+//
+//  witness_calc.h
+//  rapidsnark
+//
+//  Created by Yaroslav Moria on 26.07.2024.
+//  Copyright © 2024 CocoaPods. All rights reserved.
+//
+
+#import "graph_witness.h"
+#import "libbabyjubjub.h"
+
+#ifndef witness_calc_h
+#define witness_calc_h
+
+void* test(const char *inputs, const void *graph_data, const size_t graph_data_len);
+
+#endif /* witness_calc_h */
