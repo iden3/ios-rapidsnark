e9d5e9c2a158b408a89272316d4f42eedd40594a
Yaroslav Moria <5eeman@users.noreply.github.com>
2024-11-21 16:56:26 +0100

    Updated example.

--- Example/Pods/Target Support Files/Pods-rapidsnark_Example/Pods-rapidsnark_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-rapidsnark_Example/Pods-rapidsnark_Example-frameworks.sh
@@ -176,9 +176,11 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CircomWitnesscalc/CircomWitnesscalc.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/rapidsnark/rapidsnark.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CircomWitnesscalc/CircomWitnesscalc.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/rapidsnark/rapidsnark.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
--- Example/rapidsnark/ViewController.swift
+++ Example/rapidsnark/ViewController.swift
@@ -8,55 +8,76 @@
 
 import UIKit
 import os.log
+import UniformTypeIdentifiers
 
+import CircomWitnesscalc
 import rapidsnark
 
 class ViewController: UIViewController {
-
+    var pickedFileType = FileType.inputs
+    
+    var inputs: Data?
+    var graph: Data?
+    var zkey: Data?
+    var verificationKey: Data?
+    
+    var witness: Data?
+    
+    var proof: (proof: String, publicSignals: String)?
+    
     @IBOutlet weak var resultLabel: UILabel!;
-
+    
     @IBOutlet weak var proofLabel: UILabel!;
     @IBOutlet weak var inputLabel: UILabel!;
     @IBOutlet weak var executionTimeLabel: UILabel!;
     @IBOutlet weak var verificationTimeLabel: UILabel!;
-
+    
     override func viewDidLoad() {
         super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
+        
+        inputs = FileManager.default.contents(atPath :inputsPath())!
+        graph = FileManager.default.contents(atPath :witnessGraphPath())!
+        zkey = FileManager.default.contents(atPath :zkeyPath())!
+        verificationKey = FileManager.default.contents(atPath :verificatonKeyPath())!
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning();
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func generateProofClick(_ sender: Any) {
         generateProof(buffer:false)
     }
-
+    
     @IBAction func generateBufferProofClick(_ sender: Any) {
         generateProof(buffer:true)
     }
-
+    
     func generateProof(buffer: Bool){
         do {
             let zkeyPath = zkeyPath();
-            let witness = FileManager.default.contents(atPath: witnessPath())!;
-
+            let witness = try calculateWitness(
+                inputs: inputs!,
+                graph: graph!
+            )
+            
             let startTime = Date()
-
+            
             let proof, inputs: String
             if buffer {
                 let zkey = FileManager.default.contents(atPath: zkeyPath)!;
                 (proof, inputs) = try groth16Prove(zkey: zkey, witness: witness);
-            }else{
+            } else {
                 (proof, inputs) = try groth16ProveWithZKeyFilePath(zkeyPath: zkeyPath, witness: witness);
             }
-
+            
             let endTime = Date()
             let diffProofTime = endTime.timeIntervalSince(startTime)
-
-
+            
+            self.proof = (proof: proof, publicSignals: inputs)
+            
             let verificationKey = FileManager.default.contents(atPath :verificatonKeyPath())!
             let startVerificationTime = Date()
             let isValid = try groth16Verify(proof: proof.data(using: .utf8)!, inputs: inputs.data(using: .utf8)!, verificationKey: verificationKey)
@@ -68,14 +89,18 @@ class ViewController: UIViewController {
 
             print("Verification result: \(isValid)")
             displayExecutionResult(proof, inputs, diffProofTime, diffVerificationTime)
-
         } catch {
             resultLabel.text = "Error while calculating proof: " + String(describing: error);
             print(error)
         }
     }
-
-    func displayExecutionResult(_ proof: String, _ inputs: String, _ executionTime: Double, _ verificationTime: Double){
+    
+    func displayExecutionResult(
+        _ proof: String,
+        _ inputs: String,
+        _ executionTime: Double,
+        _ verificationTime: Double
+    ) {
         proofLabel.text = proof
         inputLabel.text = inputs
         executionTimeLabel.text = executionTime.description
@@ -86,7 +111,7 @@ class ViewController: UIViewController {
         print("Execution Time: \(executionTime) seconds")
         print("Verification Time: \(verificationTime) seconds")
     }
-
+    
     @IBAction
     func onCopyToClipboard() {
         let pasteBoard = UIPasteboard.general;
@@ -97,17 +122,101 @@ class ViewController: UIViewController {
         let alertController = UIAlertController(title: "Copied", message: "Proof and inputs have been copied to clipboard.", preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "OK", style: .default))
         self.present(alertController, animated: true)
+    }
+    
+    @IBAction
+    func onShare() {
+        if (proof == nil) {
+            return
+        }
+        
+        let file = proof!.proof.data(using: .utf8)!.dataToFile(fileName: "proof.json")
+        
+        let fileURL = NSURL(fileURLWithPath: file!.path!)
+        
+        // Create the Array which includes the files you want to share
+        let filesToShare = [fileURL]
+        
+        // Make the activityViewContoller which shows the share-view
+        let activityViewController = UIActivityViewController(activityItems: filesToShare, applicationActivities: nil)
+        
+        // Show the share-view
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+    
+    func inputsPath() -> String {
+        return Bundle.main.path(forResource: "authV2_inputs", ofType: "json")!;
+    }
+    
+    func witnessGraphPath() -> String {
+        return Bundle.main.path(forResource: "authV2", ofType: "wcd")!;
+    }
+    
+    func zkeyPath() -> String {
+        return Bundle.main.path(forResource: "authV2", ofType: "zkey")!;
+    }
+    
+    func verificatonKeyPath() -> String {
+        return Bundle.main.path(forResource: "authV2_verification_key", ofType: "json")!;
+    }
 }
 
-    func zkeyPath() -> String {
-        return Bundle.main.path(forResource: "circuit_final", ofType: "zkey")!;
+enum FileType {
+    case zkey, graph, inputs, verificationKey;
+    
+    public var name : String {
+        return switch self {
+        case .zkey: "zkey"
+        case .graph: "graph"
+        case .inputs: "inputs"
+        case .verificationKey: "verificationKey"
+        }
     }
-
-    func witnessPath() -> String {
-        return Bundle.main.path(forResource: "witness", ofType: "wtns")!;
+    
+    @available(iOS 14.0, *)
+    public var uttype: UTType {
+        switch self {
+        case .zkey:
+            return UTType.data
+        case .graph:
+            return UTType.data
+        case .inputs:
+            return UTType.json
+        case .verificationKey:
+            return UTType.json
+        }
     }
+    
+    public var documentType: String {
+        switch self {
+        case .zkey:
+            return "application/octet-stream"
+        case .graph:
+            return "application/octet-stream"
+        case .inputs:
+            return "application/json"
+        case .verificationKey:
+            return "application/json"
+        }
+    }
+}
 
-    func verificatonKeyPath() -> String {
-        return Bundle.main.path(forResource: "verification_key", ofType: "json")!;
+func getDocumentsDirectory() -> NSString {
+    let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+    let documentsDirectory = paths[0]
+    return documentsDirectory as NSString
+}
+
+extension Data {
+    func dataToFile(fileName: String) -> NSURL? {
+        let data = self
+        let filePath = getDocumentsDirectory().appendingPathComponent(fileName)
+        do {
+            try data.write(to: URL(fileURLWithPath: filePath))
+            return NSURL(fileURLWithPath: filePath)
+        } catch {
+            print("Error writing the file: \(error.localizedDescription)")
+        }
+        return nil
     }
 }
