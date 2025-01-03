d1b85d9bc7fd40603db85bea29c8180a55ed06b7
Yaroslav Moria <5eeman@users.noreply.github.com>
2025-01-03 15:39:10 +0100

    Updated example.

--- Example/rapidsnark/ViewController.swift
+++ Example/rapidsnark/ViewController.swift
@@ -13,24 +13,38 @@ import UniformTypeIdentifiers
 import CircomWitnesscalc
 import rapidsnark
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIDocumentPickerDelegate {
     var pickedFileType = FileType.inputs
     
     var inputs: Data?
     var graph: Data?
-    var zkey: Data?
+    var zkeyPath: String?
     var verificationKey: Data?
     
     var witness: Data?
     
-    var proof: (proof: String, publicSignals: String)?
+    var proof: (proof: String, inputs: String)?
     
-    @IBOutlet weak var resultLabel: UILabel!;
+    @IBOutlet
+    weak var inputsLabel: UILabel!;
     
-    @IBOutlet weak var proofLabel: UILabel!;
-    @IBOutlet weak var inputLabel: UILabel!;
-    @IBOutlet weak var executionTimeLabel: UILabel!;
-    @IBOutlet weak var verificationTimeLabel: UILabel!;
+    @IBOutlet
+    weak var graphLabel: UILabel!;
+    
+    @IBOutlet
+    weak var zkeyLabel: UILabel!;
+    
+    @IBOutlet
+    weak var verificationKeyLabel: UILabel!;
+    
+    @IBOutlet
+    weak var witnessLabel: UILabel!;
+    
+    @IBOutlet
+    weak var proofLabel: UILabel!;
+    
+    @IBOutlet
+    weak var verificationLabel: UILabel!;
     
     override func viewDidLoad() {
         super.viewDidLoad();
@@ -38,7 +52,6 @@ class ViewController: UIViewController {
         
         inputs = FileManager.default.contents(atPath :inputsPath())!
         graph = FileManager.default.contents(atPath :witnessGraphPath())!
-        zkey = FileManager.default.contents(atPath :zkeyPath())!
         verificationKey = FileManager.default.contents(atPath :verificatonKeyPath())!
     }
     
@@ -47,13 +60,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func generateProofClick(_ sender: Any) {
-        generateProof()
+    @IBAction
+    public func selectInputs() {
+        pickedFileType = FileType.inputs
+        
+        let pickerViewController = filePicker(fileType: pickedFileType)
+        self.present(pickerViewController, animated: true, completion: nil)
     }
     
-    func generateProof(){
+    @IBAction
+    public func selectGraphBinFile() {
+        pickedFileType = FileType.graph
+        
+        let pickerViewController = filePicker(fileType: pickedFileType)
+        self.present(pickerViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction
+    public func selectZkeyFile() {
+        pickedFileType = FileType.zkey
+        
+        let pickerViewController = filePicker(fileType: pickedFileType)
+        self.present(pickerViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction
+    public func selectVerificationKeyFile() {
+        pickedFileType = FileType.verificationKey
+        
+        let pickerViewController = filePicker(fileType: pickedFileType)
+        self.present(pickerViewController, animated: true, completion: nil)
+    }
+    
+    @IBAction
+    func generateProof() {
         do {
-            let zkeyPath = zkeyPath();
+            let zkeyPath = getZkeyPath();
             let witness = try calculateWitness(
                 inputs: inputs!,
                 graph: graph!
@@ -64,49 +106,109 @@ class ViewController: UIViewController {
             let (proof, inputs) = try groth16Prove(zkeyPath: zkeyPath, witness: witness);
             
             let endTime = Date()
-            let diffProofTime = endTime.timeIntervalSince(startTime)
+            let executionTime = endTime.timeIntervalSince(startTime)
             
-            self.proof = (proof: proof, publicSignals: inputs)
+            self.proof = (proof: proof, inputs: inputs)
             
-            let verificationKey = FileManager.default.contents(atPath :verificatonKeyPath())!
-            let startVerificationTime = Date()
-            let isValid = try groth16Verify(proof: proof.data(using: .utf8)!, inputs: inputs.data(using: .utf8)!, verificationKey: verificationKey)
-            let endVerificatonTime = Date()
-            let diffVerificationTime = endVerificatonTime.timeIntervalSince(startVerificationTime)
-            
-            let bufferSize = try groth16PublicBufferSize(zkeyPath: zkeyPath)
-            print("Buffer size: \(bufferSize)")
-            
-            print("Verification result: \(isValid)")
-            displayExecutionResult(proof, inputs, diffProofTime, diffVerificationTime)
+            proofLabel.text = "Execution Time: \(executionTime) seconds\n\(proof)"
+            // print result in console
+            print("Proof: \(proof)")
+            print("Inputs: \(inputs)")
+            print("Execution Time: \(executionTime) seconds")
         } catch {
-            resultLabel.text = "Error while calculating proof: " + String(describing: error);
+            proofLabel.text = "Error while calculating proof: " + String(describing: error);
             print(error)
         }
     }
     
-    func displayExecutionResult(
-        _ proof: String,
-        _ inputs: String,
-        _ executionTime: Double,
-        _ verificationTime: Double
-    ) {
-        proofLabel.text = proof
-        inputLabel.text = inputs
-        executionTimeLabel.text = executionTime.description
-        verificationTimeLabel.text = verificationTime.description
-        // print result in console
-        print("Proof: \(proof)")
-        print("Inputs: \(inputs)")
-        print("Execution Time: \(executionTime) seconds")
-        print("Verification Time: \(verificationTime) seconds")
+    @IBAction
+    func onValidateProof() {
+        do {
+            let verificationKey = verificationKey ?? FileManager.default.contents(atPath :verificatonKeyPath())!
+            let startVerificationTime = Date()
+            let isValid = try groth16Verify(
+                proof: proof!.proof.data(using: .utf8)!,
+                inputs: proof!.inputs.data(using: .utf8)!,
+                verificationKey: verificationKey
+            )
+            let endVerificatonTime = Date()
+            let verificationTime = endVerificatonTime.timeIntervalSince(startVerificationTime)
+            
+            print("Verification result: \(isValid)")
+            print("Verification Time: \(verificationTime) seconds")
+            
+            verificationLabel.text = "Verification result: \(isValid)\nVerification Time: \(verificationTime) seconds"
+        } catch {
+            proofLabel.text = "Error while verifying proof: " + String(describing: error);
+            print(error)
+        }
+    }
+    
+    public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let url = urls.first else {
+            NSLog("File pick ulrs empty or cancelled")
+            return
+        }
+        NSLog("Selected file URL: " + url.description)
+        
+        let label = switch pickedFileType {
+        case .zkey: zkeyLabel
+        case .inputs: inputsLabel
+        case .graph: graphLabel
+        case .verificationKey: verificationKeyLabel
+        }
+        
+        var data = Data()
+        do {
+            if pickedFileType == .zkey {
+                zkeyPath = url.path
+            } else {
+                data = try Data(contentsOf: url)
+            }
+        } catch {
+            label?.text = "Error picking " + pickedFileType.name + ": " + error.localizedDescription
+            NSLog(error.localizedDescription)
+            return
+        }
+        
+        label?.text = "Got " + pickedFileType.name + ". Name: " + url.lastPathComponent
+        
+        switch pickedFileType {
+        case .graph:
+            graph = data
+        case .inputs:
+            inputs = data
+        case .verificationKey:
+            verificationKey = data
+        default:
+            break
+        }
+    }
+    
+    private func filePicker(fileType: FileType) -> UIDocumentPickerViewController {
+        let pickerViewController = if #available(iOS 14.0, *) {
+            UIDocumentPickerViewController(
+                forOpeningContentTypes: [fileType.uttype],
+                asCopy: true
+            )
+        } else {
+            UIDocumentPickerViewController(
+                documentTypes: [fileType.documentType],
+                in: UIDocumentPickerMode.open
+            )
+        }
+        pickerViewController.delegate = self
+        pickerViewController.allowsMultipleSelection = false
+        if #available(iOS 13.0, *) {
+            pickerViewController.shouldShowFileExtensions = true
+        }
+        return pickerViewController
     }
     
     @IBAction
     func onCopyToClipboard() {
         let pasteBoard = UIPasteboard.general;
-        let str = (proofLabel.text ?? "") + "\n" + (inputLabel.text ?? "")
-        pasteBoard.string = str;
+        pasteBoard.string = proof.map { $0.proof + "\n" + $0.inputs }
         
         // Create and present an alert controller
         let alertController = UIAlertController(title: "Copied", message: "Proof and inputs have been copied to clipboard.", preferredStyle: .alert)
@@ -142,8 +244,8 @@ class ViewController: UIViewController {
         return Bundle.main.path(forResource: "authV2", ofType: "wcd")!;
     }
     
-    func zkeyPath() -> String {
-        return Bundle.main.path(forResource: "authV2", ofType: "zkey")!;
+    func getZkeyPath() -> String {
+        return zkeyPath ?? Bundle.main.path(forResource: "authV2", ofType: "zkey")!;
     }
     
     func verificatonKeyPath() -> String {
